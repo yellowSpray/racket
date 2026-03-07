48633a9fc0c1d2a6d47aa1ea3e90f85ba24faab7
yellowSpray <timothy35@hotmail.com>
2026-03-07 22:01:39 +0100

    refactor: update dashboard grid to 15 columns with pill sidebar
    - Change grid from 12 to 15 columns with 15px gap
    - Sidebar takes col-span-1 with w-fit, content takes col-span-14
    - Move rounded-full and border styles to aside element

--- front/src/layout/DashboardLayout.tsx
+++ front/src/layout/DashboardLayout.tsx
@@ -10,15 +10,15 @@ export default function DashboardLayout({
 
     return (
 
-    <div className="w-full flex-1 grid grid-cols-12 min-h-0">
+    <div className="w-full flex-1 grid grid-cols-15 gap-[15px] min-h-0">
 
         {sidebar && (
-            <aside className="col-span-2 md:col-span-2 lg:col-span-2">
+            <aside className="col-span-1 h-full flex flex-col rounded-full border border-border w-fit">
                 {sidebar}
             </aside>
         )}
 
-        <section className="w-full col-span-10 p-10 h-full flex flex-col min-h-0">
+        <section className="w-full col-span-14 h-full flex flex-col min-h-0 pt-7.5">
             {children}
         </section>
 
