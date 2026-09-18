18f6c28d7a93959ac69ce6ee16f0c05393eba33d
yellowSpray <timothy35@hotmail.com>
2026-09-18 04:26:26 +0200

    Passer les onglets a la coque depuis les deux pages

--- front/src/pages/admin/AdminPage.tsx
+++ front/src/pages/admin/AdminPage.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
-import { AdminSideBar } from "@/components/admin/AdminSidebar";
+import { AdminSideBar, AdminTabs } from "@/components/admin/AdminSidebar";
 import DashboardLayout from "@/layout/DashboardLayout";
 import { PlayersProvider } from "@/contexts/PlayersContext";
 
 export default function AdminPage() {
   return (
     <PlayersProvider>
-      <DashboardLayout sidebar={<AdminSideBar />}>
+      <DashboardLayout sidebar={<AdminSideBar />} onglets={<AdminTabs />}>
         <Outlet />
       </DashboardLayout>
     </PlayersProvider>
